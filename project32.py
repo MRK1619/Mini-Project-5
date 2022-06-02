8830e3c415b6293ed9e901d790f2beb32ea7c0ca
Partha Sarathi Hazra <sarathiparthahazra9609@gmail.com>
2022-06-02 19:07:47 +0530

    Random Password Generator

---
+++ project32.py
@@ -0,0 +1,6 @@
+# Random Password Generator
+import random
+passlen = int(input("Enter The Length Of Password:  "))
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p = "".join(random.sample(s,passlen ))
+print(p)
